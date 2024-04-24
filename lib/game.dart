3293ba6571dfd3ebeff1e8869b2ee93d77f3364b
Jonathan de Gaston <developerjon5@gmail.com>
2024-04-24 16:18:46 -0600

    Scrollable map

--- lib/game.dart
+++ lib/game.dart
@@ -1,21 +1,98 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 
-class MainGame extends FlameGame with HasGameRef, TapDetector {
-  static const scale = 1.0;
+class MainGame extends FlameGame with ScaleDetector, TapDetector {
   late TiledComponent mapComponent;
+  static const double _minZoom = 0.5;
+  static const double _maxZoom = 2.0;
+  double _startZoom = _minZoom;
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
+    camera.viewfinder
+      ..zoom = _startZoom
+      ..anchor = Anchor.topLeft;
 
     mapComponent = await TiledComponent.load(
-      "level1.tmx",
+      'level1.tmx',
       Vector2.all(64),
     );
+    world.add(mapComponent);
+  }
 
-    add(mapComponent);
+  @override
+  void onScaleStart(ScaleStartInfo info) {
+    _startZoom = camera.viewfinder.zoom;
+  }
+
+  @override
+  void onScaleUpdate(ScaleUpdateInfo info) {
+    final currentScale = info.scale.global;
+
+    if (currentScale.isIdentity()) {
+      // One-finger gesture
+      _processDrag(info);
+    } else {
+      // Several fingers gesture
+      _processScale(info, currentScale);
+    }
+  }
+
+  @override
+  void onScaleEnd(ScaleEndInfo info) {
+    _checkScaleBorders();
+    _checkDragBorders();
+  }
+
+  void _processDrag(ScaleUpdateInfo info) {
+    final delta = info.delta.global;
+    final zoomDragFactor = 1.0 /
+        _startZoom; // To synchronize a drag distance with current zoom value
+    final currentPosition = camera.viewfinder.position;
+
+    camera.viewfinder.position = currentPosition.translated(
+      -delta.x * zoomDragFactor,
+      -delta.y * zoomDragFactor,
+    );
+  }
+
+  void _processScale(ScaleUpdateInfo info, Vector2 currentScale) {
+    final newZoom = _startZoom * ((currentScale.y + currentScale.x) / 2.0);
+    camera.viewfinder.zoom = newZoom.clamp(_minZoom, _maxZoom);
+  }
+
+  void _checkScaleBorders() {
+    camera.viewfinder.zoom = camera.viewfinder.zoom.clamp(_minZoom, _maxZoom);
+  }
+
+  void _checkDragBorders() {
+    final worldRect = camera.visibleWorldRect;
+
+    final currentPosition = camera.viewfinder.position;
+
+    final mapSize = Offset(mapComponent.width, mapComponent.height);
+
+    var xTranslate = 0.0;
+    var yTranslate = 0.0;
+
+    if (worldRect.topLeft.dx < 0.0) {
+      xTranslate = -worldRect.topLeft.dx;
+    } else if (worldRect.bottomRight.dx > mapSize.dx) {
+      xTranslate = mapSize.dx - worldRect.bottomRight.dx;
+    }
+
+    if (worldRect.topLeft.dy < 0.0) {
+      yTranslate = -worldRect.topLeft.dy;
+    } else if (worldRect.bottomRight.dy > mapSize.dy) {
+      yTranslate = mapSize.dy - worldRect.bottomRight.dy;
+    }
+
+    camera.viewfinder.position =
+        currentPosition.translated(xTranslate, yTranslate);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
-import 'package:flutter/material.dart';
-import 'main_game_page.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/widgets.dart' hide Animation, Image;
+import 'package:military_madness/game.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'GamePad',
-      home: MainGamePage(),
-    );
-  }
+  runApp(
+    GameWidget(
+      game: MainGame(),
+    ),
+  );
 }
